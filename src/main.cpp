228799b16f4d03c85471c2d8bd716d357d265a73
Aayush Joshi <aayushjos@gmail.com>
2024-05-04 11:21:35 +0000

    caching densities done parallel with -ofast

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@ const int particle_num = 3600;
 const int particle_mass = 1;
 const int particle_radius = 5;
 const float collision_damping = 0.75f;
-double smoothing_radius = 50.f;
+const float pi = 3.141f;
+double smoothing_radius = 300.f;
 const float dt = 1.f/60.f;
 const float gravity = 9.81; //ms^-2
 
@@ -23,10 +24,10 @@ struct particle{
     sf::CircleShape droplet{particle_radius};
     sf::Vector2f position{0.f, 0.f};
     sf::Vector2f velocity{0.f, 0.f};
+    float local_density;
 };
 
 vector<particle> particles(particle_num);
-
 
 void placeParticles(){
     int particlesPerRow = sqrt(particle_num);
@@ -35,6 +36,7 @@ void placeParticles(){
     for (int i = 0; i < particle_num; i++){
         particles[i].position.x = (i % particlesPerRow + particlesPerRow / 5.f + 0.5f) * spacing;
         particles[i].position.y = (i / particlesPerRow + particlesPerColumn / 5.f + 0.5f) * spacing;
+        particles[i].droplet.setFillColor(sf::Color::Cyan); // Remove after resolveColor is developed
     }
 }
 
@@ -44,25 +46,19 @@ void resolveGravity(int i){
 }
 
 float smoothingKernel(double dst){
-    float volume = 3/(2 * smoothing_radius);  
+    float volume = pi * (float)pow(smoothing_radius, 5.0)/10;  
     float value = max((float)0.0, (float)pow(smoothing_radius - dst, 3.0));
     return value/volume;
 }
 
-/*
-float calculateDensity(sf::Vector2f sample_point){
-    float density = 0;
-    for (auto &particlet: particles){
-        float dst = sqrt(pow((double)(particlet.position.x - sample_point.x), 2.0) + pow((double)(particlet.position.y - sample_point.y), 2.0));
-        float influence = smoothingKernel(dst);
-        density += particle_mass * influence; 
-    }
-    return density;
+float smoothingKernelDerivative(double dst){
+    if (dst >= smoothing_radius) return 0.0;
+    return ( (float) -30/(pi * pow(smoothing_radius, 5)) * pow(smoothing_radius - dst, 2));
 }
-*/
+
 float calculateDensity(sf::Vector2f sample_point){
     float density = 0;
-    //#pragma omp parallel for num_threads(5) 
+    #pragma omp parallel for num_threads(9) 
     for (int j =0; j < particle_num; j++){
         float dst = sqrt(pow((double)(particles[j].position.x - sample_point.x), 2.0) + pow((double)(particles[j].position.y - sample_point.y), 2.0));
         float influence = smoothingKernel(dst);
@@ -81,7 +77,19 @@ void resolveBoundingBox(int i, sf::Vector2u window_size){
         particles[i].velocity.y *= -1 * collision_damping;   
     }
 }
-//shape.setFillColor(sf::Color::Green);
+
+/*
+TO-DO!
+void resolveColor(int i){
+    if (abs(particles[i].velocity.x + particles[i].velocity.y) > 50){
+        float offset = particles[i].velocity.x + particles[i].velocity.y - 50;
+        particles[i].droplet.setFillColor(sf::Color(2*offset , 2*offset, 250-2*offset));
+    }
+    else{
+        float offset = 50 - abs(particles[i].velocity.x) - abs(particles[i].velocity.y);
+        particles[i].droplet.setFillColor(sf::Color(2*offset, 2*offset, 250-2*offset));
+    }   
+}*/
 
 int main()
 {
@@ -91,7 +99,8 @@ int main()
     sf::View view = window.getDefaultView();
 
     placeParticles();
-
+    //cout << calculateDensity(particles[1800-30].position); show that this makes the density roughly constant
+    
     while (window.isOpen())
     {
         sf::Event event;
@@ -114,7 +123,9 @@ int main()
             // window bounding box
             resolveBoundingBox(i, window_size);
             //calculate densities
-            calculateDensity(particles[i].position);
+            particles[i].local_density = calculateDensity(particles[i].position);
+            //resolve colour
+            //resolveColor(i); add at the end
             //set particle position with radius offset 
             particles[i].droplet.setPosition(particles[i].position.x-particle_radius, particles[i].position.y-particle_radius);
 
