5e9859308a45213783cfb1f93df51b30078fba00
Aayush Joshi <aayushjos@gmail.com>
2024-05-05 16:19:35 +0000

    other particle index

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,21 @@ using namespace std;
  //Initialize fluid-related
 
 const int particle_num = 1600; 
-const int particle_mass = 1;
+const float particle_mass = 1.f;
 const int particle_radius = 5;
-const float collision_damping = 0.1f;
+const float collision_damping = 0.5f;
 const float pi = 3.141f;
 const float target_density = 0.001f;
-const float pressure_multiplier = 1000.f;
+const float pressure_multiplier = 100.f;
 double smoothing_radius = 200.f;
-const float dt = 1.f/480.f;
-const float gravity = 1000.f; 
+const float dt = 1.f/60.f;
+const float gravity = 500.f; 
 
 struct particle{
     sf::CircleShape droplet{particle_radius};
     sf::Vector2f position{0.f, 0.f};
     sf::Vector2f velocity{0.f, 0.f};
-    float local_density;
+    float local_density = 0.001f;
     float local_pressure;
 };
 
@@ -37,8 +37,8 @@ void placeParticles(){
     int particlesPerColumn = (particle_num - 1)/particlesPerRow + 1;
     int spacing = 2.5*particle_radius; 
     for (int i = 0; i < particle_num; i++){
-        particles[i].position.x = (i % particlesPerRow + particlesPerRow / 5.f + 0.5f) * spacing;
-        particles[i].position.y = (i / particlesPerRow + particlesPerColumn / 5.f + 0.5f) * spacing;
+        particles[i].position.x = (i % particlesPerRow + particlesPerRow / 2.5f +0.5f) * spacing;
+        particles[i].position.y = (i / particlesPerRow + particlesPerColumn / 2.5f +0.5f) * spacing;
         particles[i].droplet.setFillColor(sf::Color::Cyan); // Remove after resolveColor is developed
     }
 }
@@ -70,12 +70,12 @@ float smoothingKernelDerivative(double dst){
     return ((float) -30.f/(pi * pow(smoothing_radius, 5)) * pow(smoothing_radius - dst, 2));
 }
 /////////////////////////////////////////////////////////
-float calculateDensity(sf::Vector2f sample_point){
+float calculateDensity(int i){
     float density = 0.f;
     //#pragma omp parallel for num_threads(9) 
     for (int j =0; j < particle_num; j++){
        // float dst = sqrt(pow((double)(particles[j].position.x - sample_point.x), 2.0) + pow((double)(particles[j].position.y - sample_point.y), 2.0));
-        float dst = sqrt((particles[j].position.x - sample_point.x) * (particles[j].position.x - sample_point.x)  + (particles[j].position.y - sample_point.y) * (particles[j].position.y - sample_point.y));
+        float dst = sqrt((particles[j].position.x - particles[i].position.x) * (particles[j].position.x - particles[i].position.x)  + (particles[j].position.y - particles[i].position.y) * (particles[j].position.y - particles[i].position.y));
         float influence = smoothingKernel(dst);
         density += particle_mass * influence; 
     }
@@ -87,6 +87,8 @@ float densityToPressure(int j){
     float local_pressure = density_error * pressure_multiplier;
     return local_pressure;
 }
+
+
 
 sf::Vector2f calculatePressureForce(int i){
     sf::Vector2f pressure_force;
@@ -107,14 +109,12 @@ sf::Vector2f calculatePressureForce(int i){
         float gradient = smoothingKernelDerivative(dst);
         float x_dir = x_offset/dst;
         float y_dir = y_offset/dst;
-
-        pressure_force.x += densityToPressure(j) * gradient * particle_mass/particles[i].local_density * x_dir;
-        pressure_force.y += densityToPressure(j) * gradient * particle_mass/particles[i].local_density * y_dir;
+        pressure_force.x += densityToPressure(j) * gradient * particle_mass/particles[j].local_density * x_dir;
+        pressure_force.y += densityToPressure(j) * gradient * particle_mass/particles[j].local_density * y_dir;
     }
-    //cout << pressure_force.x << " " << pressure_force.y << "\n";
-    return 100.f *pressure_force;
+    return 100.f * pressure_force;
 }
-
+/*
 void resolveBoundingBox(int i, sf::Vector2u window_size){
     if (particles[i].position.x > window_size.x || particles[i].position.x < 0){
         particles[i].position.x = clamp((int)particles[i].position.x, 0, (int)window_size.x);
@@ -123,6 +123,29 @@ void resolveBoundingBox(int i, sf::Vector2u window_size){
     if (particles[i].position.y >= window_size.y || particles[i].position.y <= 0){
         particles[i].position.y = clamp((int)particles[i].position.y, 0, (int)window_size.y);
         particles[i].velocity.y *= -1 * collision_damping;   
+    }
+}*/
+
+void resolveCollisions(int i, sf::Vector2u window_size){
+    if (particles[i].position.x > window_size.x || particles[i].position.x < 0){
+        particles[i].position.x = clamp((int)particles[i].position.x, 0, (int)window_size.x);
+        particles[i].velocity.x *= -1 * collision_damping;
+    }
+    if (particles[i].position.y >= window_size.y || particles[i].position.y <= 0){
+        particles[i].position.y = clamp((int)particles[i].position.y, 0, (int)window_size.y);
+        particles[i].velocity.y *= -1 * collision_damping;
+    }
+    
+    for (int k = 0; k < particle_num; k++){
+        if (i == k) continue;
+        float x_offset = particles[i].position.x - particles[k].position.x;
+        float y_offset = particles[i].position.y - particles[k].position.y;
+        float total_offset = x_offset * x_offset + y_offset * y_offset;
+        if(total_offset < 2*particle_radius){
+            float correction = 2*particle_radius - total_offset;
+            particles[k].position.x += sqrt(correction / 2);
+            particles[k].position.y += sqrt(correction / 2);
+        }
     }
 }
 
@@ -167,11 +190,11 @@ int main()
         
         for (int i = 0; i < particle_num; i++){
             //gravity step  
-            resolveGravity(i);
+            //resolveGravity(i);
             // window bounding box
-            resolveBoundingBox(i, window_size);
+            //resolveBoundingBox(i, window_size);
             //calculate densities
-            particles[i].local_density = calculateDensity(particles[i].position);
+            particles[i].local_density = calculateDensity(i);
             //convert density to pressure
             particles[i].local_pressure = densityToPressure(i);
             //Calculate pressure forces 
@@ -188,7 +211,7 @@ int main()
             particles[i].position.x += particles[i].velocity.x * dt;
             particles[i].position.y += particles[i].velocity.y * dt;
             particles[i].droplet.setPosition(particles[i].position.x-particle_radius, particles[i].position.y-particle_radius);
-
+            resolveCollisions(i, window_size);
             window.draw(particles[i].droplet);
         }
         window.display();
